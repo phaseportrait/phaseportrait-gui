a7b71c421c9862f2a8aec618e07ad542e8b51997
Unai Lería Fortea <unicelularuni15@gmail.com>
2022-03-09 10:48:42 +0100

    Change to use global phaseportrait installation

--- phaseportrait-launcher.py
+++ phaseportrait-launcher.py
@@ -1,4 +1,4 @@
-from phaseportrait_local.phaseportrait import PhasePortrait2DManager
+from phaseportrait import PhasePortrait2DManager
 from sys import argv
 
 if __name__ == '__main__':
@@ -7,19 +7,3 @@ if __name__ == '__main__':
         '--plot' : PhasePortrait2DManager.plot_from_json
     }
     print(modes[argv[1]](argv[2]))
-    
-    
-# from phaseportrait.phaseportrait import PhasePortrait2DManager
-# from sys import argv
-
-# if __name__ == '__main__':
-#     with open('duck.txt', 'w') as file:
-#         file.write(str(argv))
-#         modes = {
-#             '--code' : PhasePortrait2DManager.json_to_python_code,
-#             '--plot' : PhasePortrait2DManager.plot_from_json
-#         }
-#         output = modes[argv[1]](argv[2])
-
-#         file.write(str(output))
-#         print(str(output))
